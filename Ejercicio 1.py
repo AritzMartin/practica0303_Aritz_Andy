b15c6dc8171076ba4c4bcaea3e0dff5959832a24
amartingir <113341165+AritzMartin@users.noreply.github.com>
2023-01-30 11:30:35 +0100

    Ejercicio 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+ventana = pygame. display.set_mode((640, 480))
+pygame.display.set_caption('Ejemplo 1')
+
+jugando = True
+while jugando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jugando = False
+    ventana.fill((255, 255, 255))
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
